2b422aae417b45b4978e28f481fc7b1a254aa79e
Jack_S76_WSL <me@jh0l.com>
2021-06-24 15:09:38 +1000

    feat(project): :sparkles: implement websocket support for sensor
    Implements webserver with a websocket endpoint that spawns a client (seperate thread) that connects to the webserver's ws endpoint. Based on actix_web_examples/websocket/websocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+
+use actix_files as fs;
+use actix_web::{middleware, web, App, HttpServer};
+
+mod sensor_websocket;
+
+use sensor_websocket::ws_index;
+
+mod sensor_client;
+
+use sensor_client::SensorClient;
+
+const ADDRESS: &str = "127.0.0.1:8080";
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
+    env_logger::init();
+
+    // initialize sqlite db if not already initialized
+
+    SensorClient::spawn(ADDRESS);
+
+    HttpServer::new(|| {
+        App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            // websocket route
+            .service(web::resource("/ws/").route(web::get().to(ws_index)))
+            // static files
+            .service(fs::Files::new("/", "static/").index_file("index.html"))
+    })
+    // start http server on 127.0.0.1:8080
+    .bind(ADDRESS)?
+    .run()
+    .await
 }
---
+++ src/sensor_client.rs
@@ -0,0 +1,120 @@
+use std::time::Duration;
+use std::{io, thread};
+
+use actix::*;
+use actix::io::SinkWrite;
+use actix_codec::Framed;
+use awc::{
+    error::WsProtocolError,
+    ws::{Codec, Frame, Message},
+    BoxedSocket, Client,
+};
+use futures::stream::{SplitSink, StreamExt};
+
+use bytes::Bytes;
+
+pub struct SensorClient {
+    sink: SinkWrite<Message, SplitSink<Framed<BoxedSocket, Codec>, Message>>,
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+struct SensorReading(String);
+
+impl Actor for SensorClient {
+    type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Context<Self>) {
+        // start heartbeats otherwise server will disconnect after 10 seconds
+        self.hb(ctx)
+    }
+
+    fn stopped(&mut self, _: &mut Context<Self>) {
+        println!("Disconnected");
+
+        // Stop application on disconnect
+        println!("STOPPING SENSOR CLIENT");
+        System::current().stop();
+    }
+}
+
+
+/// Handle stdin commands
+impl Handler<SensorReading> for SensorClient {
+    type Result = ();
+
+    fn handle(&mut self, msg: SensorReading, _ctx: &mut Context<Self>) {
+        self.sink.write(Message::Text(msg.0));
+    }
+}
+
+/// Handle server websocket messages
+impl StreamHandler<Result<Frame, WsProtocolError>> for SensorClient {
+    fn handle(&mut self, msg: Result<Frame, WsProtocolError>, _: &mut Context<Self>) {
+        if let Ok(Frame::Text(txt)) = msg {
+            println!("Server: {:?}", txt)
+        }
+    }
+
+    fn started(&mut self, _ctx: &mut Context<Self>) {
+        println!("Connected");
+    }
+
+    fn finished(&mut self, ctx: &mut Context<Self>) {
+        println!("Server disconnected");
+        ctx.stop()
+    }
+}
+
+impl actix::io::WriteHandler<WsProtocolError> for SensorClient {}
+
+/// How often heartbeat pings are sent
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+
+impl SensorClient {
+    fn hb(&self, ctx: &mut Context<Self>) {
+        ctx.run_later(HEARTBEAT_INTERVAL, |act, ctx| {
+            act.sink.write(Message::Ping(Bytes::from_static(b"")));
+            act.hb(ctx);
+
+            // client should also check for a timeout here, similar to the
+            // server code
+        });
+    }
+
+    pub fn spawn(server_url: &'static str) {
+        let server_url = &(*server_url);
+        Arbiter::spawn(async move {
+            let mut url = "http://".to_owned();
+            url.push_str(server_url);
+            url.push_str("/ws/");
+            let (response, framed) = Client::new()
+                .ws(url)
+                .connect()
+                .await
+                .map_err(|e| {
+                    println!("Error: {}", e);
+                })
+                .unwrap();
+            println!("{:?}", response);
+            let (sink, stream) = framed.split();
+            let addr = SensorClient::create(|ctx| {
+                SensorClient::add_stream(stream, ctx);
+                SensorClient {sink: SinkWrite::new(sink, ctx)}
+            });
+
+            // start sensor reading loop
+            thread::spawn(move || {
+                loop {
+                    let mut cmd = String::new();
+                    if io::stdin().read_line(&mut cmd).is_err() {
+                        println!("error");
+                        return;
+                    }
+                    // let srl = format!("eco2:{} evtoc:{} UT:{} IN:{} TS:{}", sensor.read().unwrap());
+                    addr.do_send(SensorReading(cmd));
+                }
+            });
+        });
+    }
+}
