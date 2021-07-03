fb1d5289c7fd1b3d56fb7bd16193e77426e6245c
Jack_S76_WSL <me@jh0l.com>
2021-07-04 09:55:34 +1000

    build: :hammer: add ssh only flag

--- cross_deploy.sh
+++ cross_deploy.sh
@@ -2,7 +2,7 @@
 # compile on ubuntu then copy to raspberry pi and run
 
 set -o errexit
-set -o nounset
+# set -o nounset
 set -o pipefail
 set -o xtrace
 
@@ -11,6 +11,11 @@ readonly TARGET_PATH=/home/pi/Repositories/airtest/remote_bin
 readonly TARGET_ARCH=armv7-unknown-linux-gnueabihf
 readonly SOURCE_PATH=/home/jh0/Documents/air_meter_server/target/armv7-unknown-linux-gnueabihf/release/server
 
-cross build --release --target=${TARGET_ARCH}
-scp ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
+if [ "$1" != "ssh" ]; then
+    cross build --release --target=${TARGET_ARCH}
+    scp ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
+else
+    echo only ssh
+fi
+
 ssh -t ${TARGET_HOST} ${TARGET_PATH}/server
