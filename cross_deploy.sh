bc62b0198a96b022323842a9e994216dbcd8a22e
Jack_S76_WSL <me@jh0l.com>
2021-07-10 16:32:42 +1000

    build: :hammer: transfer binary files through tar | pv| netcat client server instead of scp

--- cross_deploy.sh
+++ cross_deploy.sh
@@ -9,15 +9,15 @@ set -o xtrace
 readonly TARGET_HOST=pi@192.168.0.67
 readonly TARGET_PATH=/home/pi/Repositories/airtest/remote_bin
 readonly TARGET_ARCH=armv7-unknown-linux-gnueabihf
-readonly SOURCE_PATH=/home/jh0/Documents/air_meter_server/target/armv7-unknown-linux-gnueabihf/release/server
-readonly DIR_PATH=/home/jh0/Documents/air_meter_server/
+readonly SOURCE_PATH=target/armv7-unknown-linux-gnueabihf/release/server
 
 if [ "$1" = "ssh" ]; then
     ssh -t ${TARGET_HOST} "cd $TARGET_PATH ; exec \$SHELL -l"
 else
     cross build --release --target=${TARGET_ARCH}
-    scp ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
-    scp ${DIR_PATH}.env ${TARGET_HOST}:${TARGET_PATH}
-    scp ${DIR_PATH}server.db ${TARGET_HOST}:${TARGET_PATH}
-    ssh -t ${TARGET_HOST} ${TARGET_PATH}/server
+    # scp ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
+    # scp server.db ${TARGET_HOST}:${TARGET_PATH}
+    tar cf - server.db $SOURCE_PATH | pv | netcat 192.168.0.67 7000;
 fi
+
+# TODO add new script for moving next.js build to static folder and index template
