7d54af1c8ab17dc15ed4b1ea7e0d22b5b1f8c449
Jack_S76_WSL <me@jh0l.com>
2021-07-20 19:47:31 +1000

    style: :lipstick: add amogus

--- web_app/air_meter_client/pages/index.tsx
+++ web_app/air_meter_client/pages/index.tsx
@@ -74,17 +74,17 @@ export default function Home() {
 
             <footer className={styles.footer}>
                 <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    href="http://memes"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
                     Powered by{' '}
                     <span className={styles.logo}>
                         <img
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            width={72}
-                            height={16}
+                            src="/amogus.jpg"
+                            alt="Memes"
+                            width={30}
+                            height={30}
                         />
                     </span>
                 </a>
